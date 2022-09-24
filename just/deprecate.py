14facec91463c2cb0263ccffb93988c323456c8c
perplx <perplx@users.noreply.github.com>
2022-09-24 19:32:25 -0400

    Initial version of just.deprecate
    Contains a decorator to deprecate a function or method.

---
+++ just/deprecate.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+"""Deprecation decorator"""
+
+
+# standard imports
+import functools
+import warnings
+
+
+# TODO message
+# TODO version parameters, deprecated_since, removed-in, etc.
+# TODO invocation file, line
+def deprecated(func):
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        # save and restore warnings filter
+        with warnings.catch_warnings():
+            # enable warnings filter for DeprecationWarning
+            # it's enabled by default, but this ensures it works
+            warnings.simplefilter("always", DeprecationWarning)
+
+            # warnings module is preferrable to logging module for the problems
+            # that stem from incorrect implementation (not runtime problems)
+            warnings.warn(f"deprecated: {func.__name__}", category=DeprecationWarning, stacklevel=2)
+
+        # evaluate the wrapped function
+        return func(*args, **kwargs)
+    
+    # return the decorated function
+    return wrapped_func
+
+
+def main():
+    """simple test"""
+
+    # capture `warnings` messages into `logging` messages
+    import logging
+    logging.basicConfig(format="%(asctime)s %(name)s %(levelname)-8s %(message)s", level=logging.DEBUG)
+    logging.captureWarnings(True)
+
+    # declare deprecated function
+    @deprecated
+    def deprecated_func():
+        return None
+
+    # call deprecated function to show warning and log message
+    deprecated_func()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_deprecate.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+
+"""Unit-tests for just.deprecate"""
+
+
+# standard imports
+import unittest
+import logging
+
+# tested imports
+from just.deprecate import deprecated
+
+
+class TestDeprecated(unittest.TestCase):
+    """test for `just.deprecate.deprecated`"""
+
+    def test_deprecate_function(self):
+        """"test that a deprecated function emits a warning when called"""
+
+        # define deprecated function
+        @deprecated
+        def deprecated_func():
+            return None
+
+        # test deprecated function
+        with self.assertWarns(DeprecationWarning):
+            deprecated_func()
+
+    def test_deprecate_method(self):
+        """"test that a deprecated method emits a warning when called"""
+
+        # define class with deprecated method
+        class DeprecatedClass:
+            @deprecated
+            def deprecated_method(self):
+                return None
+        
+        # test deprecated method
+        instance = DeprecatedClass()
+        with self.assertWarns(DeprecationWarning):
+            instance.deprecated_method()
+
+    def test_deprecate_logging(self):
+        """test that a deprecated call produces a log message when warnings are captured by logging"""
+
+        # define deprecated function
+        @deprecated
+        def deprecated_func():
+            return None
+        
+        # logging to receive warnings
+        logging.captureWarnings(True)
+        LOG_PREFIX = "WARNING:py.warnings:" # line with default format
+
+        with self.assertLogs("py.warnings", level=logging.WARNING) as cm:
+            # call the deprecated function
+            deprecated_func()
+
+            # ensure log output contains 1 line
+            self.assertEqual(len(cm.output), 1)
+            output_line = cm.output[0]
+
+            # ensure log output contains LOG_PREFIX
+            self.assertIn(LOG_PREFIX, output_line)
+
+            # ensure log output starts with LOG_PREFIX
+            self.assertTrue(output_line.startswith(LOG_PREFIX))
