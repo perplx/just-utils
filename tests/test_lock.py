aa3c20da5afe1e9c73b4ecaeb60cb749dd978d78
perplx <perplx@users.noreply.github.com>
2024-07-06 12:53:49 -0400

    Added unit-tests for just.lock

---
+++ tests/test_lock.py
@@ -0,0 +1,40 @@
+"""Unit-tests for just-lock"""
+
+# standard imports
+import os
+import tempfile
+import unittest
+
+# local imports
+import just.lock
+
+
+# global constants
+LOCK_FILE_PATH = "bogusbogusbogusbogus"
+
+
+class TestLock(unittest.TestCase):
+    """test for just.utils.lock_file"""
+
+    def setUp(self):
+        """Create the temp-dir where the tested lock-file exists."""
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.lock_file = os.path.join(self.temp_dir.name, "LOCK_FILE")
+
+    def tearDown(self):
+        """Cleanup the temp-dir where the tested lock-file exists."""
+        self.temp_dir.cleanup()
+
+    def test_lock_exists(self):
+        """Test the lock-file only exists in the locked context."""
+        self.assertFalse(os.path.isfile(self.lock_file))
+        with just.lock.lock_file(self.lock_file):
+            self.assertTrue(os.path.isfile(self.lock_file))
+        self.assertFalse(os.path.isfile(self.lock_file))
+
+    def test_lock_error(self):
+        """Test that locking the same file twice causes a FileExistsError."""
+        with self.assertRaises(FileExistsError):
+            with just.lock.lock_file(self.lock_file):
+                with just.lock.lock_file(self.lock_file):
+                    pass
