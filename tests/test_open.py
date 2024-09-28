c97282f081d5b8694e2757f66379d0ce742a84b6
perplx <perplx@users.noreply.github.com>
2024-09-28 00:37:46 -0400

    Added just.open

---
+++ tests/test_open.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Unit-tests for just.timing"""
+
+
+# standard imports
+import unittest
+
+# tested imports
+import just.open
+
+
+class TestOpen(unittest.TestCase):
+    """Tests for just.open.my_open"""
+
+    TEST_TEXT = "test\n"
+
+    def test_open_bzip2(self):
+        gz_file_path = "./data/test_data.txt.bz2"
+        with just.open.ezopen(gz_file_path, mode="rt") as test_file:
+            self.assertEquals(test_file.read(), self.TEST_TEXT)
+
+    def test_open_gzip(self):
+        gz_file_path = "./data/test_data.txt.gz"
+        with just.open.ezopen(gz_file_path, mode="rt") as test_file:
+            self.assertEquals(test_file.read(), self.TEST_TEXT)
+
+    def test_open_text(self):
+        gz_file_path = "./data/test_data.txt"
+        with just.open.ezopen(gz_file_path, mode="rt") as test_file:
+            self.assertEquals(test_file.read(), self.TEST_TEXT)
