aeead6ab87f5018108682266eae1c2a80eea39e0
perplx <perplx@users.noreply.github.com>
2024-09-29 01:03:01 -0400

    fix test

--- tests/test_open.py
+++ tests/test_open.py
@@ -18,14 +18,14 @@ class TestOpen(unittest.TestCase):
     def test_open_bzip2(self):
         gz_file_path = "./data/test_data.txt.bz2"
         with just.open.ezopen(gz_file_path, mode="rt") as test_file:
-            self.assertEquals(test_file.read(), self.TEST_TEXT)
+            self.assertEqual(test_file.read(), self.TEST_TEXT)
 
     def test_open_gzip(self):
         gz_file_path = "./data/test_data.txt.gz"
         with just.open.ezopen(gz_file_path, mode="rt") as test_file:
-            self.assertEquals(test_file.read(), self.TEST_TEXT)
+            self.assertEqual(test_file.read(), self.TEST_TEXT)
 
     def test_open_text(self):
         gz_file_path = "./data/test_data.txt"
         with just.open.ezopen(gz_file_path, mode="rt") as test_file:
-            self.assertEquals(test_file.read(), self.TEST_TEXT)
+            self.assertEqual(test_file.read(), self.TEST_TEXT)
