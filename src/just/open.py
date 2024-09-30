d7559686608df45cc6d346dbbfbe528bab090a35
perplx <perplx@users.noreply.github.com>
2024-09-29 20:43:22 -0400

    added documentation

--- src/just/open.py
+++ src/just/open.py
@@ -5,20 +5,29 @@ import gzip
 from pathlib import Path
 
 
-def ezopen(file_path: str, mode: str = ""):
+def ezopen(file_path: Path | str, mode: str | None = None):
+    """Open a file whether it's compressed or not.
+    Open archive files using the appropriate compression based on the file extension.
+    Open file normally if no extension matches.
+    """
+
+    # open compressed files based on their extensions
     if file_path.endswith(".bz2"):
         return bz2.open(file_path, mode=mode)
     if file_path.endswith(".gz"):
         return gzip.open(file_path, mode=mode)
 
+    # open file normally
     return open(file_path, mode=mode)
 
 
 def main() -> None:
+    # test data
     text_file_path = Path("./tests/data/test_data.txt").absolute()
     bz2_file_path = Path("./tests/data/test_data.txt.bz2").absolute()
     gz_file_path = Path("./tests/data/test_data.txt.gz").absolute()
 
+    # test each case
     with ezopen(text_file_path, "rt") as text_file:
         print(text_file.read())
     with ezopen(bz2_file_path, "rt") as bz2_file:
