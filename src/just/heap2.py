cbde109904ea2ba21dae14eb4f30c1c01a7ea662
perplx <perplx@users.noreply.github.com>
2026-06-29 00:25:04 -0400

    Typed Heap implementation

---
+++ src/just/heap2.py
@@ -0,0 +1,46 @@
+"""Now with type annotations"""
+
+import heapq
+from typing import Collection, Generic, TypeVar
+
+
+T = TypeVar("T")
+
+
+# FIXME support maxheap? (vesrion of python...)
+# FIXME support key-function without type complications
+# FIXME support pushpop, extra operations, __str__, etc.
+# FIXME tests!!!!
+class Heap(Generic[T]):
+    def __init__(self, data: Collection[T]):
+        self._heap = list(data)  # FIXME copy or take>
+        heapq.heapify(self._heap)
+
+    def __len__(self) -> int:
+        """Number of items in the `Heap`."""
+        return len(self._heap)
+    
+    def peek(self) -> T:
+        """FIXME"""
+        top = self._heap[0]
+        return top
+    
+    def pop(self) -> T:
+        """FIXME"""
+        item = heapq.heappop(self._heap)
+        return item
+    
+    def push(self, item: T) -> None:
+        """FIXME"""
+        heapq.heappush(self._heap, item)
+    
+
+def main() -> None:
+    """Simple test."""
+    l = [1,2,6,3,4,1,7,9]
+    heap = Heap(l)
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
