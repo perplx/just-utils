bb33ed618e9564ccee63fb639f13822c002a735e
perplx <perplx@users.noreply.github.com>
2026-08-06 23:20:28 -0400

    Format bytes count as string

--- src/just/human.py
+++ src/just/human.py
@@ -8,8 +8,40 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+IEC_UNITS = [
+    "B",
+    "KiB",  # kibibyte 1024^1
+    "MiB",  # mebibyte 1024^2
+    "GiB",  # gibibyte 1024^3
+    "TiB",  # tebibyte 1024^4
+    "PiB",  # pebibyte 1024^5
+    "EiB",  # exbibyte 1024^6
+    "ZiB",  # zebibyte 1024^7
+    "YiB",  # yobibyte 1024^8
+]
+
+LS_UNITS = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+
+
+def find_blocks(num_bytes: int, block_size: int) -> tuple[float, int]:
+    if num_bytes < 0:
+        raise ValueError("num_bytes {num_bytes} should be positive")
+    num_blocks = 0
+    while num_bytes >= block_size:
+        num_bytes /= block_size
+        num_blocks += 1
+    return num_bytes, num_blocks
+
+
 def format_bytes(num_bytes: int) -> str:
-    raise NotImplementedError
+    if num_bytes < 0:
+        raise ValueError("num_bytes {num_bytes} should be positive")
+    num_bytes, num_blocks = find_blocks(num_bytes, 1024)
+    if num_bytes < 10.0:
+        bytes_str = f"{num_bytes:.1f}"
+    else:
+        bytes_str = f"{num_bytes:.0f}"
+    return f"{bytes_str}{LS_UNITS[num_blocks]}"
 
 
 def parse_bytes(num_bytes: str) -> int:
@@ -67,6 +99,13 @@ def main() -> None:
     """Simple test."""
 
     logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.NOTSET)
+
+    print(format_bytes(1023))
+    print(format_bytes(1024))
+    print(format_bytes(1023 * 1024))
+    print(format_bytes(1024 * 1024))
+
+    raise SystemExit
 
     seconds = 100_000
     print(seconds)
