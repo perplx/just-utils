9ee3f71cd6bfcd02083d901bfed19a6e5d5804bb
perplx <perplx@users.noreply.github.com>
2026-08-09 13:10:25 -0400

    Updated tests for just.first

--- tests/test_first.py
+++ tests/test_first.py
@@ -7,22 +7,31 @@
 import unittest
 
 # tested imports
-from just.first import first_condition, first_next
+from just.first import first
 
 
 class TestFirst(unittest.TestCase):
     """test for `just.first`"""
 
-    def test_first_next(self):
-        """test `just.first.first_next`"""
+    def test_first(self):
+        """test `just.first.first` with no condition."""
 
-        self.assertEqual(first_next([]), None)
-        self.assertEqual(first_next([0, 0, 0]), None)
-        self.assertEqual(first_next([0, 1, 0]), 1)
+        self.assertEqual(first([], default=None), None)
+        self.assertEqual(first([0, 0, 0], default=None), None)
+        self.assertEqual(first([0, 1, 0], default=None), 1)
 
     def test_first_condition(self):
-        """test `just.first.first_condition`"""
+        """test `just.first.first` with condition."""
 
-        self.assertEqual(first_condition([0, 1, 2, 3], lambda x: x), 1)
-        self.assertEqual(first_condition([0, 1, 2, 3], lambda x: x % 3 == 2), 2)
-        self.assertEqual(first_condition([None, 0, 1], lambda x: x is not None), 0)
+        self.assertEqual(first([0, 1, 2, 3], lambda x: x), 1)
+        self.assertEqual(first([0, 1, 2, 3], lambda x: x % 3 == 2), 2)
+        self.assertEqual(first([None, 0, 1], lambda x: x is not None), 0)
+
+    def test_first_missing(self):
+        """test `just.first.first` with no matching item."""
+
+        with self.assertRaises(ValueError):
+            self.assertEqual(first([]), None)
+        with self.assertRaises(ValueError):
+            self.assertEqual(first([0, 0, 0]), None)
+
